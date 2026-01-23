a120d3d6d0f78830f7f93b22b2ac3a72285c6e8d
AyushSri5 <156398511+AyushSri5@users.noreply.github.com>
2026-01-23 21:33:36 +0530

    Reverse String

---
+++ Reverse_String.java
@@ -0,0 +1,23 @@
+// Two pointers TC: O(n) SC: O(1)
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int i=0;
+        int j=nums.length-1;
+        while(i<=j){
+            while(i<=j && nums[i]!=val){
+                i++;
+            }
+            while(i<=j && nums[j]==val){
+                j--;
+            }
+            if(i<j){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            i++;
+            j--;
+            }
+        }
+        return i;
+    }
+}
