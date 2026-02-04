18edf74d322082268685c43f6b1f2989159caf17
AyushSri5 <156398511+AyushSri5@users.noreply.github.com>
2026-02-04 21:52:05 +0530

    Split string

---
+++ Split_string.java
@@ -0,0 +1,16 @@
+// Optimised approach TC: O(n) SC: O(1)
+class Solution {
+    public int balancedStringSplit(String s) {
+        int ans=0;
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='L')
+            ans++;
+            else
+            ans--;
+            if(ans==0)
+            count++;
+        }
+        return count;
+    }
+}
