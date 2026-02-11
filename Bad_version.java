cfd897bd08f6e76a8074ccae2f0388732ba353ae
AyushSri5 <156398511+AyushSri5@users.noreply.github.com>
2026-02-11 09:19:02 +0530

    First Bad Version

---
+++ Bad_version.java
@@ -0,0 +1,18 @@
+// Optimised approach TC: O(log(n)) SC: O(1)
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int start=1;
+        int end=n;
+        int ans=-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(isBadVersion(mid)){
+                ans=mid;
+                end=mid-1;
+            }
+            else
+            start=mid+1;
+        }
+        return ans;
+    }
+}
