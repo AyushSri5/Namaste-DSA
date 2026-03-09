9cc65f4f36ac3f775c9e988869e66a33ee3e91d8
AyushSri5 <156398511+AyushSri5@users.noreply.github.com>
2026-03-09 13:41:09 +0530

    Subtree inside a tree

---
+++ Subtree_tree.java
@@ -0,0 +1,19 @@
+// Optimised approach TC: O(N*M) SC: O(N)
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root == null) return false;
+
+        if(isSame(root, subRoot)) return true;
+
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    public boolean isSame(TreeNode a, TreeNode b){
+        if(a == null && b == null) return true;
+        if(a == null || b == null) return false;
+
+        if(a.val != b.val) return false;
+
+        return isSame(a.left,b.left) && isSame(a.right,b.right);
+    }
+}
